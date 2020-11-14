7128012b853f88fd892a165bd0a86c7f569cb0dd
Dario Santos <dario.santos@fortbrasil.com.br>
2020-11-14 10:16:08 -0300

    Configuracao na estrutura do projeto

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,14 @@
+import {Platform} from 'react-native';
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({
+    host: Platform.OS === 'ios' ? 'localhost' : '10.0.0.240',
+  })
+    .useReactNative()
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
